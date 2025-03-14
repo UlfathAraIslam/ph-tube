62728fe769366c74cdce0d02f45641ca3f7e0f57
UlfathAraIslam <mouliajsahid@gmail.com>
2025-03-14 20:48:33 +0900

    categories are loaded dynamically

---
+++ script/index.js
@@ -0,0 +1,28 @@
+function loadCategories() {
+  // fetch data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    // get a promise and convert it in json
+    .then((res) => res.json())
+    // send data to displayCategories
+    .then((data) => displayCategories(data.categories));
+}
+function displayCategories(categories) {
+  // get the container
+  const categoryContainer = document.getElementById("category-container");
+
+  // loop operation on array of object
+
+  for (let category of categories) {
+    console.log(category);
+
+    // create element
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+    <button class="btn btn-sm">${category.category}</button>
+    `
+
+    // append the element
+    categoryContainer.append(categoryDiv)
+  }
+}
+loadCategories();
